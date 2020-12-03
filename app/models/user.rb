2502d64e10af07e27b528f52fb54ab166ebd916c
mattyplo <matthew.ploszaj@seattlecentral.edu>
2020-12-02 21:51:57 -0800

    Messages resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   validates :username, presence: true, length: { minimum: 3, maximum: 25 }, uniqueness: { case_sensitive: true }
   has_secure_password
+  has_many :messages
 end
