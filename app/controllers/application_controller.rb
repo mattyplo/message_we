5b068201b99c64a29e1bc56ef199e127e257a3ca
mattyplo <matthew.ploszaj@seattlecentral.edu>
2020-12-05 09:01:15 -0800

    Login Logout and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end 
+
+  def logged_in?
+    # The double exclamation marks turn the current_user method / variable into a boolean
+    !!current_user
+  end
+
+  def require_user
+    if !logged_in?
+      flash[:alert] = "You must be logged in to perform that action"
+      redirect_to login_path
+    end
+  end
 end
